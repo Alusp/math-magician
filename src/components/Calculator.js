1c211d80e3f9e5b3bd500a4616381c0adefca36f
Aseni <alusinejalloh800@gmail.com>
2022-09-08 09:48:05 +0300

    Update design and Fix Logic Calculation

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,220 +1,212 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.myHandleClick = this.myHandleClick.bind(this);
-  }
+const Calculator = () => {
+  const [state, setState] = useState({});
 
-  myHandleClick(event) {
-    this.setState((state) => calculate(state, event.target.textContent));
-  }
-
-  render() {
-    const { next, total } = this.state;
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="w-2/6 shadow-lg">
-          <div className="text-white bg-gray-500/90 flex justify-end p-5 w-full">
-            <span>{next || total || 0}</span>
-          </div>
-          <table className="w-full">
-            <tbody className="w-2/6">
-              <tr>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    AC
-                  </button>
-                </td>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    +/-
-                  </button>
-                </td>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    %
-                  </button>
-                </td>
-                <td className="p-6 border hover:bg-gray-200 text-center bg-orange-500/100 text-xl">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    ÷
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    7
-                  </button>
-                </td>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    8
-                  </button>
-                </td>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    9
-                  </button>
-                </td>
-                <td className="p-6 border hover:bg-gray-200 text-center bg-orange-500/100 text-xl">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    *
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    4
-                  </button>
-                </td>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    5
-                  </button>
-                </td>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    6
-                  </button>
-                </td>
-                <td className="p-6 border hover:bg-gray-200 text-center bg-orange-500/100 text-xl">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    -
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    1
-                  </button>
-                </td>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    2
-                  </button>
-                </td>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    3
-                  </button>
-                </td>
-                <td className="p-6 border hover:bg-gray-200 text-center bg-orange-500/100 text-xl">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    +
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-            <tfoot>
-              <tr>
-                <td
-                  colSpan="2"
-                  className="p-6 bg-white border hover:bg-gray-200 text-center"
-                >
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    0
-                  </button>
-                </td>
-                <td className="p-6 bg-white border hover:bg-gray-200 text-center">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    .
-                  </button>
-                </td>
-                <td className="p-6 border hover:bg-gray-200 text-center bg-orange-500/100 text-xl">
-                  <button
-                    type="button"
-                    className="p-6"
-                    onClick={this.myHandleClick}
-                  >
-                    =
-                  </button>
-                </td>
-              </tr>
-            </tfoot>
-          </table>
+  const myHandleClick = (event) => {
+    setState(calculate(state, event.target.textContent));
+  };
+  const { next, total } = state;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-2/6 shadow-lg">
+        <div className="text-white bg-gray-500 flex justify-end p-5 w-full">
+          <span>{next || total || 0}</span>
         </div>
+        <table className="w-full">
+          <tbody className="w-2/6">
+            <tr>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  AC
+                </button>
+              </td>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  +/-
+                </button>
+              </td>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  %
+                </button>
+              </td>
+              <td className="border hover:bg-gray-200 text-center bg-orange-500/90 text-xl">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  ÷
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  7
+                </button>
+              </td>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  8
+                </button>
+              </td>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  9
+                </button>
+              </td>
+              <td className="border hover:bg-gray-200 text-center bg-orange-500/90 text-xl">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  x
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  4
+                </button>
+              </td>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  5
+                </button>
+              </td>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  6
+                </button>
+              </td>
+              <td className="border hover:bg-gray-200 text-center bg-orange-500/90 text-xl">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  -
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  1
+                </button>
+              </td>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  2
+                </button>
+              </td>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  3
+                </button>
+              </td>
+              <td className="border hover:bg-gray-200 text-center bg-orange-500/90 text-xl">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  +
+                </button>
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td
+                colSpan="2"
+                className="bg-white border hover:bg-gray-200 text-center"
+              >
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  0
+                </button>
+              </td>
+              <td className="bg-white border hover:bg-gray-200 text-center">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  .
+                </button>
+              </td>
+              <td className="border hover:bg-gray-200 text-center bg-orange-500/90 text-xl">
+                <button
+                  type="button"
+                  className="p-6"
+                  onClick={myHandleClick}
+                >
+                  =
+                </button>
+              </td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
-    );
-  }
-}
-
+    </div>
+  );
+};
 export default Calculator;
