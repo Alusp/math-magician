5c699f2e54160180f196d115d69c45c9c50b3de0
Aseni <alusinejalloh800@gmail.com>
2022-09-14 13:22:00 +0300

    Add `NavigationBar.js file`

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavigationBar() {
+  return (
+    <div className="py-6 px-16 shadow-lg flex justify-between">
+      <Link to="/" className="text-3xl font-semibold text-orange-500">
+        Math Magician
+      </Link>
+      <div className="flex">
+        <Link to="/" exact className="mx-6 hover:text-orange-500 text-xl">
+          Home
+        </Link>
+        <Link to="/calculator" exact className="mx-6 hover:text-orange-500 text-xl">
+          Calculator
+        </Link>
+        <Link to="/quote" exact className="ml-6 hover:text-orange-500 text-xl">
+          Quotes
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NavigationBar;
