3352dedada25536e3fabe8253174cc056f6dba0a
Aseni <alusinejalloh800@gmail.com>
2022-09-14 13:24:50 +0300

    Add `QuotesScreen.js File`

---
+++ src/pages/QoutesScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function QoutesScreen() {
+  return (
+    <div className="container mx-auto py-32 flex justify-center items-center">
+      <p className="text-3xl">
+        Mathematics is not about numbers, equations computations
+        <br />
+        or algorithms: it is about understanding.
+        <span className="font-semibold"> -William Shakespare</span>
+      </p>
+    </div>
+  );
+}
+
+export default QoutesScreen;
